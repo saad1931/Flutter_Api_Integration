847583b1ef31ea31209978f4d603713d1114687d
Muhammad Saad <muhammadsaad3119@gmail.com>
2022-12-07 22:13:10 +0500

    Photos_api

---
+++ lib/models/photos_model.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class PhotosModel {
+  String title;
+  String url;
+  int id;
+  PhotosModel({
+    required this.title,
+    required this.url,
+    required this.id,
+  });
+}
--- lib/screens/post_api.dart
+++ lib/screens/post_api.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:restapi/models/post_model.dart';
